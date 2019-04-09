a8d69aa4a11afb6b63581747b40edff0491f287b
Din Lanh <langdinhieu@gmail.com>
2019-04-08 22:39:03 -0400

    added survey list components

---
+++ client/src/app/surveys/survey-delete/survey-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-delete',
+  templateUrl: './survey-delete.component.html',
+  styleUrls: ['./survey-delete.component.css']
+})
+export class SurveyDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
